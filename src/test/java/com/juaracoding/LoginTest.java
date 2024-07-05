2dfeda3fdc8c281d341bd48c39c1182023c371a2
Koswara <koswara.c79@gmail.com>
2024-07-05 19:32:59 +0700

    method snake case

--- src/test/java/com/juaracoding/LoginTest.java
+++ src/test/java/com/juaracoding/LoginTest.java
@@ -12,7 +12,7 @@ public class LoginTest {
     WebDriver driver;
 
     @Given("The user navigates to the SauceDemo login page")
-    public void theUserNavigatesToTheSauceDemoLoginPage() {
+    public void the_user_navigates_to_the_saucedemo_login_page() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
@@ -23,25 +23,25 @@ public class LoginTest {
     }
 
     @When("The user enters username {string} and password {string}")
-    public void theUserEntersUsernameAndPassword(String username, String password) {
+    public void the_user_enters_username_and_password(String username, String password) {
         driver.findElement(By.id("user-name")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
     }
 
     @And("The user clicks on the login button")
-    public void theUserClicksOnTheLoginButton() {
+    public void the_user_clicks_on_the_login_button() {
         driver.findElement(By.id("login-button")).click();
     }
 
     @Then("The user should see the products page with title {string}")
-    public void theUserShouldSeeTheProductsPageWithTitle(String expectedTitle) {
+    public void the_user_should_see_the_products_page_with_title(String expectedTitle) {
         String actualTitle = driver.findElement(By.className("title")).getText();
         Assert.assertEquals(actualTitle, expectedTitle);
         driver.quit();
     }
 
     @Then("The user should see the error message {string}")
-    public void theUserShouldSeeTheErrorMessage(String expectedErrorMessage) {
+    public void the_user_should_see_the_error_message(String expectedErrorMessage) {
         String actualErrorMessage = driver.findElement(By.cssSelector("[data-test='error']")).getText();
         Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage));
         driver.quit();
